075268cbdc0777f5d15bafa58ddb0b622d37175e
Beapsp <beasps@gmail.com>
2021-11-13 15:30:22 +0100

    sabado

--- src/cleaning_functions.py
+++ src/cleaning_functions.py
@@ -1,4 +1,7 @@
 import requests
+from functools import reduce
+import operator
+
 
 
 def geocode(direccion):
@@ -10,3 +13,23 @@ def geocode(direccion):
         return {"type": "Point", "coordinates": [data["latt"], data["longt"]]}
     except:
         return data
+
+
+def getFromDict(diccionario,mapa):
+    return reduce(operator.getitem,mapa,diccionario)
+
+def type_point(lista):
+    return {"type":"Point", "coordinates": lista}
+
+def extraetodo(json):
+    todo = {"nombre": ["name"], "latitud": ["location", "lat"], "longitud": ["location", "lng"]} 
+    total = []
+    for elemento in json:
+        libre = {key: getFromDict(elemento, value) for key,value in todo.items()}
+        libre["location"] = type_point([libre["latitud"], libre["longitud"]])
+        total.append(libre)
+    return total
+
+def suma_requisitos(lista1,lista2,lista3,lista4):
+    return [*lista1,*lista2,*lista3,*lista4]
+    
