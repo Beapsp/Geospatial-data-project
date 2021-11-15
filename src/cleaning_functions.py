7286b0d415daec6ec0d392e25f25e1e0dc9ccc9f
Beapsp <beasps@gmail.com>
2021-11-15 21:54:53 +0100

    lunes

--- src/cleaning_functions.py
+++ src/cleaning_functions.py
@@ -1,6 +1,9 @@
 import requests
 from functools import reduce
 import operator
+import pandas as pd
+from scipy.spatial import distance
+
 
 
 
@@ -30,6 +33,30 @@ def extraetodo(json):
         total.append(libre)
     return total
 
-def suma_requisitos(lista1,lista2,lista3,lista4):
-    return [*lista1,*lista2,*lista3,*lista4]
-    
+def suma_requisitos(df1,df2,df3,df4):
+    return pd.concat([df1, df2, df3, df4])
+
+def pesos(df):
+    lista_pesos=[]
+    for i,row in df.iterrows():   
+        if row["categoria"] == "Disco":
+            lista_pesos.append(row["Normalizado"]*0.2)
+        elif row["categoria"] == "Airport":
+            lista_pesos.append(row["Normalizado"]*0.4)
+        elif row["categoria"] == "Vegan":
+            lista_pesos.append(row["Normalizado"]*0.1)
+        else: 
+            lista_pesos.append(row["Normalizado"]*0.3)
+    return lista_pesos
+
+
+def normalizacion(df,col):
+    norm = []
+    for i,row in df.iterrows():
+        mini = df[col].min()
+        maxi = df[col].max()
+        norm.append((row[col]- mini)/(maxi-mini))
+    return norm
+        
+def suma_final(df1,df2,df3):
+    return pd.concat([df1, df2, df3])
